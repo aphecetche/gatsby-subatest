8b27e5932a051af9d53cac3490e435d4cb923d63
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-08-11 14:44:38 +0200

    Update typography plugin list to be able to play with themes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,12 @@
 import Typography from "typography"
-//import typo from 'typography-theme-sutro';
+// import typo from "typography-theme-sutro"
 import typo from "typography-theme-moraga"
-// moragaTheme
-// fairy-gates
-// sutro
+// import typo from "typography-theme-fairy-gates"
+// import typo from "typography-theme-lawton"
+// import typo from "typography-theme-lincoln"
+// import typo from "typography-theme-stern-grove"
+// import typo from "typography-theme-twin-peaks"
+// import typo from "typography-theme-de-young"
 const typography = new Typography(typo)
 
 export default typography

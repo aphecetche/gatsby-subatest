f1ad8f34379f33abdd7d2df0b94136b06641e11a
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-08-19 16:28:20 +0200

    Aside component for right menus

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,15 +2,23 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
+import Aside from "../components/aside"
 
 const Post = ({ data }) => {
   const { mdx } = data
   const { frontmatter, body } = mdx
 
+  let extra =
+    frontmatter.asides &&
+    frontmatter.asides.map((a, i) => <Aside key={i} regexp={a} />)
+
   return (
     <Layout>
-      <h1>{frontmatter.title}</h1>
-      <MDXRenderer>{body}</MDXRenderer>
+      {extra}
+      <main>
+        <h1>{frontmatter.title}</h1>
+        <MDXRenderer>{body}</MDXRenderer>
+      </main>
     </Layout>
   )
 }
@@ -20,6 +28,7 @@ export const query = graphql`
     mdx(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        asides
       }
       body
     }

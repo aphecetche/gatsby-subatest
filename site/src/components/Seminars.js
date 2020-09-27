d3146d766e8234300b234fce438ec86bf0efdb77
Jacopo Ghiglieri <jacopo.ghiglieri@gmail.com>
2020-09-27 19:41:59 +0200

    the seminar component is starting to show some output. Still need to figure out date manipulation

--- site/src/components/Seminars.js
+++ site/src/components/Seminars.js
@@ -1,11 +1,49 @@
 import React from "react"
-import { useTranslation } from "gatsby-theme-intl"
+// import { useTranslation } from "gatsby-theme-intl"
 
-const Seminars = () => {
-  const { t } = useTranslation()
-  const what = t("séminaires").toLowerCase()
-  const msg = t("future_list", { what })
-  return <p>{msg}</p>
+// const Seminars = () => {
+//   const { t } = useTranslation()
+//   const what = t("séminaires").toLowerCase()
+//   const msg = t("future_list", { what })
+//   return <p>{msg}</p>
+import {StaticQuery, graphql } from "gatsby"
+import Layout from "components/Layout"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+export default function Seminars() {
+  return (
+    <table>
+    <StaticQuery
+      query={graphql`
+      query {
+        allMdx(filter: {frontmatter: {category: {eq: "seminar"}}},
+        sort: { order: ASC, fields: [frontmatter___date] }) {
+           edges {
+           node {
+             frontmatter
+             {title
+             author
+             date
+             location
+             type
+           }
+           body
+           }
+         }
+       }
+      }
+      `}
+      render={data => (
+        data.allMdx.edges.map(({ node }, index) => (
+          <tr>
+          <td>{node.frontmatter.date}</td>
+          <td>{node.frontmatter.title}</td>
+          </tr>
+                ))
+      )}
+    />
+    </table>
+  )
 }
 
-export default Seminars
+

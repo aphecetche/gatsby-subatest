37fb1edd6bb920bf6fe704c5dd497d21fedbfa4c
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-11-16 13:44:24 +0100

    First version of seminars

---
+++ site/src/templates/seminar.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable react/display-name */
+
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../components/Layout"
+import { makeStyles, Grid } from "@material-ui/core"
+import { MDXProvider } from "@mdx-js/react"
+import DebugComponents from "../components/DebugComponents"
+import moment from "moment";
+import { useTranslation,usePageContext } from "gatsby-theme-intl";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    ...theme.typography.body1,
+  },
+  aside: {
+    "& ul": {
+      listStyle: "none",
+      border: "1px solid red",
+    },
+  },
+  main: {
+    margin: 0,
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    "& p": {
+      ...theme.typography.body1,
+      textAlign: "justify",
+      textJustify: "inter-character",
+    },
+  },
+}))
+
+const SeminarLayout = ({ data }) => {
+  const { mdx } = data
+  const { frontmatter, body } = mdx
+  const classes = useStyles()
+  const { language: currentLanguage } = usePageContext();
+  const { t } = useTranslation()
+  moment.locale(currentLanguage)
+  return (
+    <MDXProvider components={DebugComponents}>
+      <Layout>
+        <Grid container className={classes.root}>
+          <Grid item xs={false} sm={1} />
+          <Grid item xs={12} sm={10} className={classes.main}>
+            <main>
+            <h1 style={{textTransform: 'capitalize'}}>{t(frontmatter.type)}</h1>
+            <h2>{moment(frontmatter.date,"YYYY-MM-DD HH:mm:ss Z").format("dddd, MMMM Do YYYY, HH:mm")}, {frontmatter.location}</h2>
+              {frontmatter.title && <h3> {frontmatter.title}</h3>}
+              {frontmatter.author &&<h4> {frontmatter.author_url && <a href={frontmatter.author_url} target="_blank" rel="noreferrer" style={{textTransform: 'capitalize'}}>{frontmatter.author.toLowerCase()}</a>}
+            {frontmatter.author_url ==="" && <span style={{textTransform: 'capitalize'}}>{frontmatter.author.toLowerCase()}</span>},&nbsp;
+            {frontmatter.author_filiation_url && <a href={frontmatter.author_filiation_url} target="_blank" rel="noreferrer">{frontmatter.author_filiation}</a>  }
+            {frontmatter.author_filiation_url ==="" && frontmatter.author_filiation  }
+            </h4>}
+            {frontmatter.title2 && <h3>{frontmatter.title2}</h3>}
+              {frontmatter.author2 &&<h4> {frontmatter.author_url2 && <a href={frontmatter.author_url2} target="_blank" rel="noreferrer" style={{textTransform: 'capitalize'}}>{frontmatter.author2.toLowerCase()}</a>  }
+            {frontmatter.author_url2 ==="" && <span style={{textTransform: 'capitalize'}}>{frontmatter.author2.toLowerCase()}</span>},&nbsp;
+            {frontmatter.author_filiation_url2 && <a href={frontmatter.author_filiation_url2} target="_blank" rel="noreferrer">{frontmatter.author_filiation2}</a>  }
+            {frontmatter.author_filiation_url2 ==="" && frontmatter.author_filiation2  }
+            </h4>}
+
+            
+
+              <MDXRenderer>{body}</MDXRenderer>
+            </main>
+          </Grid>
+          <Grid item xs={false} sm={1} />
+        </Grid>
+      </Layout>
+    </MDXProvider>
+  )
+}
+
+export const query = graphql`
+  query seminarLayout($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        title2
+        layout
+        author
+        author_url
+        author2
+        author_url2
+        date
+        location
+        type
+        author_filiation
+        author_filiation_url
+        author_filiation2
+        author_filiation_url2
+      }
+      body
+      slug
+    }
+  }
+`
+
+export default SeminarLayout

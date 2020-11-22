7125f732ae38bd8d5496881cfff8d6ba0577fb66
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-11-22 21:47:37 +0100

    Remove unused import

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -6,7 +6,7 @@ import ToggleDarkMode from "./ToggleDarkMode"
 import nav from "./navigation.json"
 import nav_en from "./navigation.en.json"
 import { AppBar, Toolbar } from "@material-ui/core"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import { usePageContext } from "gatsby-theme-intl"
 import DevOnly from "components/DevOnly"
 
